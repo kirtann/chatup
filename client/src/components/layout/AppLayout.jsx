43f7172fd70665d556379ace860f5f2e71334461
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-02 23:56:38 +0530

    update on chat page

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Title from "../shared/Title";
 import { Grid } from "@mui/material";
+import ChatList from "../specific/ChatList";
+import { sampleChats } from "../../constants/sampleData";
+import { useParams } from "react-router-dom";
 
 const AppLayout = () => (WrappedComponent) => {
   return (props) => {
+    const params = useParams();
+    const chatId = params.chatId;
+    const handleDeleteChat = (e, _id, groupChat) => {
+      e.preventDefault();
+      console.log("Delete Chat", _id, groupChat);
+    };
     return (
       <>
         <Title />
@@ -19,7 +28,11 @@ const AppLayout = () => (WrappedComponent) => {
             }}
             height={"100%"}
           >
-            First
+            <ChatList
+              chats={sampleChats}
+              chatId={chatId}
+              handleDeleteChat={handleDeleteChat}
+            />
           </Grid>
           <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
             <WrappedComponent {...props} />
---
+++ client/src/constants/sampleData.js
@@ -0,0 +1,16 @@
+export const sampleChats = [
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Ghost",
+    _id: "1",
+    groupChat: false,
+    members: ["1", "2", "3", "4"],
+  },
+  {
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "Devil",
+    _id: "2",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+];
