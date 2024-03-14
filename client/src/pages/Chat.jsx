3282e2dff4cb012ab7b2a2b8be92db524a0de11c
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-14 17:12:14 +0530

    basic home and chat layout complete

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useRef } from "react";
 import AppLayout from "../components/layout/AppLayout";
+import { IconButton, Stack } from "@mui/material";
+import { grayColor, orange } from "../constants/color";
+import {
+  AttachFile as AttachFileIcon,
+  Send as SendIcon,
+} from "@mui/icons-material";
+import { InputBox } from "../components/styles/StyledComponents";
 
 const Chat = () => {
+  const containerRef = useRef(null);
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
+    <>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      ></Stack>
+      <form style={{ height: "10%" }}>
+        <Stack
+          direction={"row"}
+          height={"100%"}
+          p={"1rem"}
+          alignItems={"center"}
+          position={"relative"}
+        >
+          <IconButton
+            sx={{
+              position: "absolute",
+              left: "1.5rem",
+              rotate: "30deg",
+            }}
+          >
+            <AttachFileIcon />
+          </IconButton>
+          <InputBox placeholder="Type Message Here..." />
+          <IconButton
+            type="submit"
+            sx={{
+              backgroundColor: `${orange}`,
+              color: "white",
+              rotate: "-30deg",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover": {
+                backgroundColor: "error.dark",
+              },
+            }}
+          >
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
+    </>
   );
 };
 
