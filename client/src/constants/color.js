6b56d5a13a8fafb3ece3ff1a01318a0d65b292de
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-14 19:11:06 +0530

    started on group page design

--- client/src/constants/color.js
+++ client/src/constants/color.js
@@ -2,3 +2,4 @@ export const orange = "#ea7070";
 export const grayColor = "rgba(247, 247, 247, 1)";
 export const lightBlue = "#2694ab";
 export const lightGreen = "#aed581";
+export const matBlack = "rgba(0,0,0,0.8)";
