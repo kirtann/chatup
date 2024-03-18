a83a0c395d7e722294b558d8907d57f0dde35388
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-18 23:03:52 +0530

    admin dashboard design update

--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -38,17 +38,17 @@ const adminTabs = [
   },
   {
     name: "Users",
-    path: "/admin/users-management",
+    path: "/admin/users",
     icon: <ManageAccountsIcon />,
   },
   {
     name: "Chats",
-    path: "/admin/chats-management",
+    path: "/admin/chats",
     icon: <GroupsIcon />,
   },
   {
     name: "Messages",
-    path: "/admin/messages-management",
+    path: "/admin/messages",
     icon: <MessageIcon />,
   },
 ];
---
+++ client/src/pages/admin/ChatManagement.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+
+const ChatManagement = () => {
+  return <AdminLayout>ChatManagement ChatManagement</AdminLayout>;
+};
+
+export default ChatManagement;
