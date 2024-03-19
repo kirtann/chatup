f046447437efa293eaa3e7b75ec9d23c60056408
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-19 15:36:28 +0530

    admin pages completed

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -79,3 +79,99 @@ export const sampleMessage = [
     createdAt: "2024-03-12T10:41:30.630Z",
   },
 ];
+
+export const dashboardData = {
+  users: [
+    {
+      name: "Ghost",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "1",
+      username: "ghost",
+      friends: 10,
+      groups: 2,
+    },
+    {
+      name: "Devil",
+      avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      _id: "2",
+      username: "devil",
+      friends: 5,
+      groups: 1,
+    },
+  ],
+
+  chats: [
+    {
+      name: "Group 1",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "1",
+      groupChat: false,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "3", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "4", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 10,
+      totalMessages: 100,
+      creator: {
+        name: "Ghost",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+    {
+      name: "Group 2",
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      _id: "2",
+      groupChat: true,
+      members: [
+        { _id: "1", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "2", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "3", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+        { _id: "4", avatar: "https://www.w3schools.com/howto/img_avatar.png" },
+      ],
+      totalMembers: 5,
+      totalMessages: 50,
+      creator: {
+        name: "Devil",
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+      },
+    },
+  ],
+  messages: [
+    {
+      attachments: [
+        // {
+        //   public_id: "asdfsa",
+        //   url: "https://www.w3schools.com/howto/img_avatar.png",
+        // },
+      ],
+      content: "Message Example",
+      _id: "dfjlsdjfljsdlfjjf",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Ghost",
+      },
+      chat: "chatId",
+      groupChat: false,
+      createdAt: "2024-02-12T10:41:30.630Z",
+    },
+    {
+      attachments: [
+        {
+          public_id: "asdfsa 2",
+          url: "https://www.w3schools.com/howto/img_avatar.png",
+        },
+      ],
+      content: "",
+      _id: "dfjlsdjfljsdlfjjffsd",
+      sender: {
+        avatar: "https://www.w3schools.com/howto/img_avatar.png",
+        name: "Devil",
+      },
+      chat: "chatId",
+      groupChat: true,
+      createdAt: "2024-03-12T10:41:30.630Z",
+    },
+  ],
+};
