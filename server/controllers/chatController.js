db28f36ef1ba929bde682331ea52f632a9266076
kirtann <94744255+kirtann@users.noreply.github.com>
2024-03-25 17:58:28 +0530

    Added add and remove member in group routes

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -2,6 +2,7 @@ import { ALERT, REFETCH_CHATS } from "../constants/events.js";
 import { getOtherMember } from "../lib/helper.js";
 import { tryCatch } from "../middlewares/error.js";
 import { Chat } from "../models/chatModel.js";
+import { User } from "../models/userModel.js";
 import { emitEvent } from "../utils/features.js";
 import { ErrorHandler } from "../utils/utility.js";
 
@@ -18,7 +19,7 @@ const newGroupChat = tryCatch(async (req, res, next) => {
   await Chat.create({
     name,
     groupChat: true,
-    creator: req.user,
+    creator: req.userId,
     members: allMembers,
   });
 
@@ -64,4 +65,123 @@ const getMyChats = tryCatch(async (req, res, next) => {
   });
 });
 
-export { newGroupChat, getMyChats };
+const getMyGroups = tryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.userId,
+    groupChat: true,
+    creator: req.userId,
+  }).populate("members", "name avatar");
+
+  const groups = chats.map(({ members, _id, groupChat, name }) => ({
+    _id,
+    groupChat,
+    name,
+    avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
+  }));
+
+  return res.status(200).json({
+    success: true,
+    groups,
+  });
+});
+
+const addMembers = tryCatch(async (req, res, next) => {
+  const { chatId, members } = req.body;
+
+  if (!members || members.length < 1)
+    return next(new ErrorHandler("Please provide members", 400));
+
+  const chat = await Chat.findById(chatId);
+
+  if (!chat) return next(new ErrorHandler("Chat not found", 404));
+
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat", 400));
+
+  if (chat.creator.toString() !== req.userId.toString())
+    return next(new ErrorHandler("You are not allowed to add members", 403));
+
+  const allNewMembersPromise = members.map((i) => User.findById(i, "name"));
+
+  const allNewMembers = await Promise.all(allNewMembersPromise);
+
+  const uniqueMembers = allNewMembers
+    .filter((i) => !chat.members.includes(i._id.toString()))
+    .map((i) => i._id);
+
+  chat.members.push(...uniqueMembers);
+
+  if (chat.members.length > 100)
+    return next(new ErrorHandler("Group members limit reached", 400));
+
+  await chat.save();
+
+  const allUsersName = allNewMembers.map((i) => i.name).join(",");
+
+  emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${allUsersName} has been added in the group`
+  );
+
+  emitEvent(req, REFETCH_CHATS, chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "Members added successfully",
+  });
+});
+
+const removeMember = tryCatch(async (req, res, next) => {
+  const { userId, chatId } = req.body;
+
+  const [chat, userThatWillBeRemoved] = await Promise.all([
+    Chat.findById(chatId),
+    User.findById(userId, "name"),
+  ]);
+
+  if (!chat) return next(new ErrorHandler("Chat not found", 404));
+
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat", 400));
+
+  if (chat.creator.toString() !== req.userId.toString())
+    return next(new ErrorHandler("You are not allowed to add members", 403));
+
+  if (chat.members.length <= 3)
+    return next(
+      new ErrorHandler("Group must have at least three members", 400)
+    );
+
+  chat.members = chat.members.filter((member) => {
+    member.toString() !== userId.toString();
+  });
+
+  await chat.save();
+
+  emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${userThatWillBeRemoved.name} has been removed from the group`
+  );
+
+  emitEvent(req, REFETCH_CHATS, chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "Member removed successfully",
+  });
+});
+
+const leaveGroup = tryCatch(async (req, res, next) => {});
+
+export {
+  newGroupChat,
+  getMyChats,
+  getMyGroups,
+  addMembers,
+  removeMember,
+  leaveGroup,
+};
--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { getMyChats, newGroupChat } from "../controllers/chatController.js";
+import {
+  addMembers,
+  getMyChats,
+  getMyGroups,
+  leaveGroup,
+  newGroupChat,
+  removeMember,
+} from "../controllers/chatController.js";
 
 const app = express.Router();
 
@@ -11,5 +18,19 @@ app.use(isAuthenticated);
 app.post("/new", newGroupChat);
 
 app.get("/my", getMyChats);
+
+app.get("/my/groups", getMyGroups);
+
+app.put("/addmembers", addMembers);
+
+app.put("/removemember", removeMember);
+
+app.delete("leave/:id", leaveGroup);
+
+// Send Attachments
+
+// Get Messages
+
+// Get Chat Details, rename, delete
 
 export default app;
